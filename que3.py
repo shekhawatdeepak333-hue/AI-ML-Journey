5e4f9dc9146e3ff9e3a888b3ff694d51ca5760cb
Deepak Shekhawat <deepakshekhawat@Deepaks-MacBook-Air.local>
2026-08-30 03:34:51 +0530

    Added que3

---
+++ que3.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Employee(ABC):
+    @abstractmethod
+    def calc_salary(self):
+        pass
+
+class Intern(Employee):
+    def calc_salary(self):
+        return "intern salary is 12000"
+
+
+class FullTimeEmployee(Employee):
+    def calc_salary(self):
+        return "FullTimeEmployee salary is 24000"
+
+class ContractEmployee(Employee):
+    def calc_salary(self):
+        return "ContractEmployee salary is 36000"
+
+E1=Intern()
+print(E1.calc_salary())
